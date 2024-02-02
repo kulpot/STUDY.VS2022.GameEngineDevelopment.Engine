f6a5027f0f8f4cd0f4b69c34c5d8179ddd8e363a
Kulpot <kulpotwahu@gmail.com>
2024-02-02 17:58:58 +0800

    ...

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -4,7 +4,20 @@
 //---------------------- Initial Folder Setup ------------------------
 //ref link:https://www.youtube.com/watch?v=pNsqHoE49eg
 
+/*
+* --------------setting up TortoiseSVN---------------------------
+* 
+* It's very important to prepare TortoiseSVN BEFORE creating any of the Visual Studio projects, otherwise it will probably be a nightmare (or at least it was for me).
+
+1) After installing TortoiseSVN, create a repository folder. This has to be an empty folder. I think this folder will store all of the data for different versions of your project.
+2) Right click and "SVN Checkout" the folder you plan on saving your Visual Studio project files in. In this video, Jamie uses D:\MyEngine\ProjectFiles. The "URL of repository" is the file that you made in step 1, and the Checkout Directory should be the folder you are trying to Checkout. The format for the URL of repository is "file:///" and then the file path, i.e. "file:///C:/MyEngine/MyRepository/". Note the 3 slashes after "file:".
+* 
+* I thought there would be more steps, but uh.. after you do these two things, everything should function properly as it does in this video (ability to Add.. and Commit) once you're created the project files.
+* 
+*/
 //->ConfigProperties->IntermediateDirectory->$(Configuration)\Tmp\
+
+
 
 void main()
 {
